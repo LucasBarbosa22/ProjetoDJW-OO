7d941e081b6f46c06ec5970a5069bfca95416c66
Lucas Barreto Barbosa <101645521+LucasBarbosa22@users.noreply.github.com>
2022-06-20 21:02:43 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = 600;
+const height = canvas.height = 400;
+
+// gera um número aleatório
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 4)) + min;
+}
+
+// gera uma cor aleatória
+
+function randomRGB() {
+  return `rgb(${random(0, 15)},${random(0, 255)},${random(0, 255)})`;
+}
+
+
+//define um vetor de bolas
+const quadrados = [];
+
+while (quadrados.length < 10) {
+   const size = random(5,10);
+   const quadrado = new Quadrado(
+      // posição de sempre uma bola de distância
+      // fora das bordas para evitar erros de desenho
+      random(0 + size,width - size),
+      random(0 + size,height - size),
+      random(-7,7),
+      random(-7,7),
+      randomRGB(),
+      size
+   );
+
+   //atualiza o vetor
+  quadrados.push(quadrado);
+}
+
+//realiza um loop em todas as bolas geradas
+function loop() {
+   ctx.fillStyle = '#05507C';
+   ctx.fillRect(1000, 0,  width, height);
+
+   for (const quadrado of quadrados) {
+    quadrado.draw();
+    quadrado.update();
+    quadrado.collisionDetect(quadrados);
+   }
+
+   requestAnimationFrame(loop);
+}
+
+loop();
